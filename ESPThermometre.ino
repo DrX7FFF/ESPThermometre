b200e1c0f23a8aaef435cf941fc26ba6d1af573c
DrX7FFF <dubourg.v@gmail.com>
2020-05-30 18:55:22 +0200

    Mesure Digitale + Ana

--- ESPThermometre.ino
+++ ESPThermometre.ino
@@ -1,43 +1,67 @@
 // https://randomnerdtutorials.com/esp8266-adc-reading-analog-values-with-nodemcu/
-
-//#include <ESP8266WiFi.h>
-//#include <ESP8266WebServer.h>
-//#include <WiFiManager.h>        //https://github.com/tzapu/WiFiManager
+#include "DHTesp.h" // library DHT sensor library for ESPx https://github.com/beegee-tokyo/DHTesp
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>        //https://github.com/tzapu/WiFiManager
+#include <ArduinoOTA.h>
 
 #define SYSTEMNAME "TEST"
-
+#define PORT 8888
+#define DELTATEMP 0.2
+#define DELTASENSOR 20
+#define RESENDMSEC 300000
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 
+WiFiUDP udp;
+DHTesp dht;
+IPAddress broadcastIP;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Start");
 
-//  WiFiManager wifiManager;
-//  wifiManager.autoConnect(SYSTEMNAME);
+  WiFiManager wifiManager;
+//  wifiManager.resetSettings();
+  wifiManager.autoConnect(SYSTEMNAME);
 
-//  Serial.println("Ready");
+  ArduinoOTA.setHostname(SYSTEMNAME);
+  ArduinoOTA.begin();
+
+  dht.setup(4, DHTesp::DHT22); // Connect DHT sensor to GPIO 4
+
+  IPAddress myIP = WiFi.localIP();
+  IPAddress myMask = WiFi.subnetMask();
+  Serial.println(myIP);
+  Serial.println(myMask);
+  for(int i = 0; i<4; i++)
+    broadcastIP[i] = (myIP[i] & myMask[i]) | (~ myMask[i]);
+  Serial.println(broadcastIP);
+  Serial.printf("UDP port : %d/r/n",PORT);
+
+  udp.begin(PORT);
 }
 
-ADC_MODE(ADC_VCC);
-
-int sensorValue = 0;  // value read from the pot
-int outputValue = 0;  // value to output to a PWM pin
-float tempValue = 0;
 void loop() {
-  sensorValue = analogRead(analogInPin);
-    // map it to the range of the PWM out
-  outputValue = map(sensorValue, 0, 1024, 0, 255);
-  tempValue = ((float)sensorValue) *11/64 - 70;
-  // print the readings in the Serial Monitor
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.print("\t output = ");
-  Serial.print(outputValue);
-  Serial.print("\t Temp = ");
-  Serial.print(tempValue);
-  Serial.print("\t Alim = ");
-  Serial.print(ESP.getVcc());
-  Serial.println();
-  delay(1000);
+  float lastSendTemp = 2000;
+  int lastSendSensor = 0;
+  unsigned long lastSendMillis;
+  
+  ArduinoOTA.handle();  
+  delay(dht.getMinimumSamplingPeriod());
+  
+  float temp = dht.getTemperature();
+  int sensor = analogRead(analogInPin);
+
+  if ((fabs(lastSendTemp - temp) > DELTATEMP) || 
+      (abs(lastSendSensor - sensor) > DELTASENSOR) ||
+      (millis() - lastSendMillis > RESENDMSEC)) {
+    lastSendTemp = temp;
+    lastSendSensor = sensor;
+    lastSendMillis = millis();
+
+    float tempAna = sensor*0.1-30.4;
+
+    udp.beginPacket(broadcastIP,PORT);
+    udp.printf("{\"mes1\":%3.1f,\"mes2\":%d,\"mes3\":%3.1f}", temp, sensor, tempAna);
+    udp.endPacket();
+  }
 }
